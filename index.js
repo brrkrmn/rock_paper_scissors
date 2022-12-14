9c4aa7ef58f1b9ebc2b2e2c0bb0dbf00c1720aab
Berra Karaman <esma.berra.karaman@hotmail.com>
2022-12-14 15:05:19 +0300

    edit js

--- index.js
+++ index.js
@@ -1,16 +1,10 @@
-const buttons = document.querySelector(".buttons").children;
-let playerSelection = null;
-let playerScores = document.querySelector(".header").children; 
-
-let you = 0, computer = 0;
-
 function addClickEventToButtons() {
     for (const button of buttons) {
         button.addEventListener("click", (event) => {
-            playerSelection = button.value;
-            let winner = playRound(playerSelection);
+            const playerSelection = button.value;
+            const winner = playRound(playerSelection);
             countScore(winner)
-            announceWinner(you, computer);
+            announceWinner();
         });
     }
 }
@@ -28,30 +22,28 @@ function getComputerChoice() {
 }
 
 function getWinner(playerSelection, computerSelection) {
-    let winner
     if (playerSelection === computerSelection) {
         document.querySelector(".result").textContent = ("Tie!");
-        winner = null;
+        return null;
     } else if (playerSelection === "rock" && computerSelection == "paper") {
         document.querySelector(".result").textContent = ("You Lose! Paper beats Rock");
-        winner = "computer";
+        return "computer";
     } else if (playerSelection === "rock" && computerSelection == "scissors") {
         document.querySelector(".result").textContent = ("You win! Rock beats Scissors");
-        winner = "you";
+        return "you";
     } else if (playerSelection === "paper" && computerSelection == "rock") {
         document.querySelector(".result").textContent = ("You win! Paper beats rock");
-        winner = "you";
+        return "you";
     } else if (playerSelection === "paper" && computerSelection == "scissors") {
         document.querySelector(".result").textContent = ("You lose! Scissors beat paper");
-        winner = "computer";
+        return "computer";
     } else if (playerSelection === "scissors" && computerSelection == "rock") {
         document.querySelector(".result").textContent = ("You lose! Rock beats scissors");
-        winner = "computer";
+        return "computer";
     } else {
         document.querySelector(".result").textContent = ("You win! Scissors beat paper");
-        winner = "you";
+        return "you";
     }
-    return winner;
 }
 
 function playRound(playerSelection) {
@@ -61,16 +53,17 @@ function playRound(playerSelection) {
 }
 
 function countScore(winner) {
+    const playerScores = document.querySelector(".header").children; 
     if (winner === "you") {
-        you ++;
+        you++;
         playerScores[0].textContent = ("You: " + you);
     } else if (winner === "computer") {
-        computer ++;
+        computer++;
         playerScores[1].textContent = ("Computer: " + computer);
     }
 }
 
-function announceWinner(you, computer) {
+function announceWinner() {
     if (you === 3 || computer === 3) {
         for (button of buttons) {
             button.setAttribute("disabled", "");
@@ -79,5 +72,7 @@ function announceWinner(you, computer) {
     }
 }
 
+const buttons = document.querySelector(".buttons").children;
+let you = 0, computer = 0;
 addClickEventToButtons();
 
